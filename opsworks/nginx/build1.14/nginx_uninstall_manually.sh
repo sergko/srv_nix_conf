daf6c7a005449b450bffc883693840a8c92efb33
Sergey Kovbyk <kovbyk@gmail.com>
2018-04-22 15:11:36 +0300

    nginx_uninstal_manually.sh fixed

--- opsworks/nginx/build1.14/nginx_uninstall_manually.sh
+++ opsworks/nginx/build1.14/nginx_uninstall_manually.sh
@@ -2,4 +2,4 @@
 test ! -d '/etc/nginx' || rm -Rf '/etc/nginx'
 test ! -f '/usr/sbin/nginx' || rm -Rf '/usr/sbin/nginx'
 test ! -d '/var/log/nginx' || rm -Rf '/var/log/nginx'
-test ! -d '/usr/lib/nginx/modules' || rm -Rf '/usr/lib/nginx/modules'
+test ! -d '/usr/lib/nginx' || rm -Rf '/usr/lib/nginx'
